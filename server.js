2210119abbf6a44b3731dd18abd1aa4f5f7d0dfe
michael-38 <michael.38@outlook.com>
2018-01-23 21:50:21 +0000

    added redirect when a form is submitted

---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080; // .env file yet to be created
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+var exportedFunctions = require("./app.js");
+
+app.set('view engine', 'ejs');
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  // console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
+
+
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  res.redirect(longURL);
+});
+
+
+app.post("/urls", (req, res) => {
+  // add new key-value pair to urlDatabase
+  randomString = exportedFunctions.generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+
+  console.log(randomString);
+  console.log(req.body.longURL);
+  console.log(urlDatabase);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(301, `http://localhost:8080/urls/${randomString}`);
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Currently listening on port ${PORT}!`);
+});
