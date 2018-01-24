f4aed14a7aa47ebb578434f097211870d3d2c0b6
michael-38 <michael.38@outlook.com>
2018-01-24 17:11:57 +0000

    added comments in file

--- server.js
+++ server.js
@@ -12,68 +12,48 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  let templateVars = { urls: urlDatabase }; //declare a variable that is an object (with a key urls) containing another object (urlDatabase)
+  res.render("urls_index", templateVars); //render urls_index and passing the ejs/html page with templateVars
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new"); //render the urls_new ejs/html page when a request to /urls/new is received
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]; //declear a variable that references the urlDatabase (an object), with the key req.params.shortURL
+  res.redirect(longURL); //redirect to the original URL
+});
 
-// app.get("/urls/:id", (req, res) => {
-//   let templateVars = { shortURL: req.params.id };
-//   // console.log(templateVars);
-//   res.render("urls_show", templateVars);
-// });
 
 
 app.post("/urls/:id", (req, res) => {
-  let templateVars = { urls: [req.params.id,urlDatabase[req.params.id]]};
-  res.render("urls_show", templateVars);
+  let templateVars = { urls: [req.params.id,urlDatabase[req.params.id]]}; //declare a variable (which is an object with key "urls") that contains an array that consists of the shortURL random string (:id) and its corresponding original URL in urlDatabase
+  res.render("urls_show", templateVars); //render the urls_show ejs/html page where user can edit the URL associated with a specific :id
 });
 
-
-app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
-  console.log(urlDatabase);
-  res.redirect("/urls");
+app.post("/urls/:id/update", (req, res) => { //POST route when user clicks Update button on the urls_show page
+  urlDatabase[req.params.id] = req.body.updatedURL; //update the urlDatabase with the updatedURL a user inputted
+  res.redirect("/urls"); //redirect to the /urls (home) page
 })
 
-
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];;
-  res.redirect(longURL);
+app.post("/urls/:id/delete", (req, res) => { //POST route when a user clicks Delete button on the urls_index (home) page
+  delete urlDatabase[req.params.id]; //delete an entry from urlDatabase
+  res.redirect("/urls"); //redirect to the /urls (home) page
 });
 
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  // console.log(req);
-  // console.log(req.params.id);
-  // console.log(urlDatabase);
-  //c onsole.log("app post id/delete");
-  res.redirect("/urls");
+app.post("/urls", (req, res) => { //POST route when user clicks Submit button on the urls_new page to create a new short URL
+  randomString = exportedFunctions.generateRandomString(); //declare a variable with a randomly generated string
+  urlDatabase[randomString] = req.body.longURL; // add new key-value pair to urlDatabase
+  let templateVars = { urls: urlDatabase }; //declare a variable that is an object (with a key urls) containing another object (urlDatabase)
+  res.render("urls_index", templateVars); //render urls_index and passing the ejs/html page with templateVars
 });
-
-app.post("/urls", (req, res) => {
-  // add new key-value pair to urlDatabase
-  randomString = exportedFunctions.generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
-
-  // console.log(randomString);
-  // console.log(req.body.longURL);
-  console.log(urlDatabase);
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-  // res.redirect(301, `http://localhost:8080/urls/${randomString}`);
-});
-
-
-
 
 
 app.listen(PORT, () => {
-  console.log(`Currently listening on port ${PORT}!`);
+  console.log(`Currently listening on port ${PORT}!`); //to indicate server is listening to the correct port
 });
+
+
