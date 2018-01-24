3efab8c8ad9f90c578cf0ca1490664faa8fc94de
michael-38 <michael.38@outlook.com>
2018-01-24 20:55:08 +0000

    added POST route for logout

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080; // .env file yet to be created
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser')
+const PORT = process.env.PORT || 8080; // .env file yet to be created
 const bodyParser = require("body-parser");
+const exportedFunctions = require("./app.js");
+
 app.use(bodyParser.urlencoded({extended: true}));
-var exportedFunctions = require("./app.js");
+app.use(cookieParser())
 
 app.set('view engine', 'ejs');
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,12 +18,19 @@ var urlDatabase = {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase }; //declare a variable that is an object (with a key urls) containing another object (urlDatabase)
+  let templateVars = {  //declare a variable that is an object
+    urls: urlDatabase, //containing an object with key as urls and value as urlDatabase
+    username: req.cookies["username"] //containing another object with key as username and value as req.cookies["username"]
+   };
   res.render("urls_index", templateVars); //render urls_index and passing the ejs/html page with templateVars
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new"); //render the urls_new ejs/html page when a request to /urls/new is received
+  let templateVars = {  ////declare a variable (which is an object)
+    urls: [req.params.id,urlDatabase[req.params.id]], // with key "urls" that contains an array that consists of the shortURL random string (:id) and its corresponding original URL in urlDatabase
+    username: req.cookies["username"] //containing an object with key as username and value as req.cookies["username"]
+    };
+  res.render("urls_new", templateVars); //render the urls_new ejs/html page when a request to /urls/new is received
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -29,8 +40,12 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 
+
 app.post("/urls/:id", (req, res) => {
-  let templateVars = { urls: [req.params.id,urlDatabase[req.params.id]]}; //declare a variable (which is an object with key "urls") that contains an array that consists of the shortURL random string (:id) and its corresponding original URL in urlDatabase
+  let templateVars = {  ////declare a variable (which is an object)
+    urls: [req.params.id,urlDatabase[req.params.id]], // with key "urls" that contains an array that consists of the shortURL random string (:id) and its corresponding original URL in urlDatabase
+    username: req.cookies["username"] //containing an object with key as username and value as req.cookies["username"]
+    };
   res.render("urls_show", templateVars); //render the urls_show ejs/html page where user can edit the URL associated with a specific :id
 });
 
@@ -44,13 +59,33 @@ app.post("/urls/:id/delete", (req, res) => { //POST route when a user clicks Del
   res.redirect("/urls"); //redirect to the /urls (home) page
 });
 
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");  //clear cookies
+  res.redirect("/urls");
+})
+
+
 app.post("/urls", (req, res) => { //POST route when user clicks Submit button on the urls_new page to create a new short URL
   randomString = exportedFunctions.generateRandomString(); //declare a variable with a randomly generated string
   urlDatabase[randomString] = req.body.longURL; // add new key-value pair to urlDatabase
-  let templateVars = { urls: urlDatabase }; //declare a variable that is an object (with a key urls) containing another object (urlDatabase)
+  let templateVars = {  //declare a variable that is an object
+    urls: urlDatabase, //containing an object with key as urls and value as urlDatabase
+    username: req.cookies["username"] //containing another object with key as username and value as req.cookies["username"]
+   };
   res.render("urls_index", templateVars); //render urls_index and passing the ejs/html page with templateVars
 });
 
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  let templateVars = {  //declare a variable that is an object
+    urls: urlDatabase, //containing an object with key as urls and value as urlDatabase
+    username: req.cookies["username"] //containing another object with key as username and value as req.cookies["username"]
+   };
+  // res.render("urls_index", templateVars);
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Currently listening on port ${PORT}!`); //to indicate server is listening to the correct port
